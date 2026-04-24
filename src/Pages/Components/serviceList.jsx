b9050a25f7dc422ba9dc884ee1c2b559eea044ba
Christopher Flores <Christopher321flores@gmail.com>
2026-04-24 07:45:54 -0400

    adding all new files

---
+++ src/Pages/Components/serviceList.jsx
@@ -0,0 +1,7 @@
+export const serviceList = [
+    { id: 1, title: "Serenatas", text: "La serenata como servicio ofrece música en vivo personalizada para celebrar momentos especiales con emoción y tradición." },
+    { id: 2, title: "Bodas", text: "La música en vivo para bodas aporta una atmósfera de elegancia y distinción, realzando cada instante de la celebración con emotividad y sofisticación." },
+    { id: 3, title: "Fiestas", text: "Para fiestas, se crea un ambiente vibrante y distinguido que eleva la celebración con energía, estilo y sofisticación." },
+    { id: 4, title: "Funerales", text: "Para servicios fúnebres, se brinda un acompañamiento solemne y respetuoso que honra la memoria del ser querido con sensibilidad y recogimiento." },
+    { id: 5, title: "Eventos", text: "Para eventos cotidianos, se crea un ambiente cálido y agradable que realza cada momento con sencillez y buen gusto." }
+];
