90bba6498f30c9c1d846f17861f82b3c237a3b05
Christopher Flores <Christopher321flores@gmail.com>
2026-06-10 16:08:21 -0400

    adding posters

--- src/Pages/Components/videoList.jsx
+++ src/Pages/Components/videoList.jsx
@@ -1,9 +1,12 @@
 import wedding from '../../media/Weddings.mp4'
 import birthdays from '../../media/HappyBirthday.mp4'
 import mass from '../../media/VirgenMorenita.mp4'
+import weddingPoster from '../../media/posters/Weddings.mp4.png'
+import birthdaysPoster from '../../media/posters/HappyBirthday.mp4.png'
+import massPoster from '../../media/posters/VirgenMorenita.mp4.png'
 
 export const videoList = [
-    {id: 0, link: wedding, name:"Bodas"},
-    {id: 1, link: birthdays, name:"Cumpleaños"},
-    {id: 2, link: mass, name:"Misas"},
+    {id: 0, link: wedding, poster: weddingPoster, name:"Bodas"},
+    {id: 1, link: birthdays, poster: birthdaysPoster, name:"Cumpleaños"},
+    {id: 2, link: mass, poster: massPoster, name:"Misas"},
 ];
